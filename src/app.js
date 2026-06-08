e22e791c3774c570ca21082797dd3981b96e7b73
Anmol vishwakrma <anmol.vishwakrma@cloudanalogy.com>
2026-06-08 14:54:57 +0530

    deplyemnt ready edit config package

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-	origin: config.clientUrl || true,
+	origin: config.clientUrl && config.clientUrl.trim() !== '' ? config.clientUrl : false,
 	credentials: true
 }));
 app.use(express.json());
